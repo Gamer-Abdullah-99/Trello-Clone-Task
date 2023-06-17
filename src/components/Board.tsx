aeacf8c2b989380078f007c9f99de173c2f1707f
Syed Abdullah <sabn730@gmail.com>
2023-06-17 22:57:57 +0500

    small changes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { AiOutlineClose } from "react-icons/ai";
 
 type TodoType = {
